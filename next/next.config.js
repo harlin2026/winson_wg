30cdc47abb9ded84e324c48bd7be2babf319bd19
WINSONODC\hans.huang <779665358@qq.com>
2026-05-15 18:27:17 +0800

    修改next.config.js

---
+++ next/next.config.js
@@ -0,0 +1,13 @@
+const nextConfig = {
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com"
+      }
+    ]
+  }
+};
+
+module.exports = nextConfig;
